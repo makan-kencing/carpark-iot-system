526249a5effc1d2f761fec84aa1bc794aeaf338e
tanyj0612 <tanyijia9558@gmail.com>
2026-04-13 22:40:06 +0800

    fix : close the esp 32 light

--- src/smart-parking-space/src/main.h
+++ src/smart-parking-space/src/main.h
@@ -1,18 +1,18 @@
-/*
- * SPDX-FileCopyrightText: 2021-2024 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier:  LicenseRef-Included
- *
- * Zigbee HA_on_off_light Example
- *
- * This example code is in the Public Domain (or CC0 licensed, at your option.)
- *
- * Unless required by applicable law or agreed to in writing, this
- * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied.
- */
+// /*
+//  * SPDX-FileCopyrightText: 2021-2024 Espressif Systems (Shanghai) CO LTD
+//  *
+//  * SPDX-License-Identifier:  LicenseRef-Included
+//  *
+//  * Zigbee HA_on_off_light Example
+//  *
+//  * This example code is in the Public Domain (or CC0 licensed, at your option.)
+//  *
+//  * Unless required by applicable law or agreed to in writing, this
+//  * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+//  * CONDITIONS OF ANY KIND, either express or implied.
+//  */
 #include "esp_zigbee_core.h"
-#include "light_driver.h"
+// #include "light_driver.h"
 
 /* Zigbee configuration */
 #define INSTALLCODE_POLICY_ENABLE       false                                /* enable the install code policy for security */
@@ -44,3 +44,4 @@
     {                                                           \
         .host_connection_mode = ZB_HOST_CONNECTION_MODE_NONE,   \
     }
+
