7b6e2d8efe9bf9d791b8c9579abff898ab4e9e2a
makan-kencing <dizzynight2096@gmail.com>
2026-04-17 14:16:36 +0800

    feat: create table on start

--- src/backend/api/src/carpark_iot_api/application.py
+++ src/backend/api/src/carpark_iot_api/application.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from dependency_injector import providers
 from fastapi import FastAPI
 from picamera2.encoders import MJPEGEncoder
@@ -8,6 +10,12 @@ from carpark_iot_api.config import CarparkSettings
 from carpark_iot_api.containers import ApplicationContainer
 from carpark_iot_core.core import Carpark
 from carpark_iot_core.db.database import AsyncDatabase
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await app.container.db().create_database()  # noqa
+    yield
 
 
 def create_app() -> FastAPI:
