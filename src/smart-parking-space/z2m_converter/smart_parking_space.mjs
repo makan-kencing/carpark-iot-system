fa9e8db30f34d3fa4a507854f028ff1d6b6feb1d
makan-kencing <dizzynight2096@gmail.com>
2026-04-21 02:04:08 +0800

    feat: update reporting attribute for z2m

--- src/smart-parking-space/z2m_converter/smart_parking_space.mjs
+++ src/smart-parking-space/z2m_converter/smart_parking_space.mjs
@@ -18,14 +18,24 @@ export const definitions = [
                 cluster: "genAnalogInput",
                 attribute: "maxPresentValue",
                 description: "The total space supported by this parking sensor",
-                access: "STATE_GET"
+                access: "STATE_GET",
+                reporting: {
+                    min: "MIN",
+                    max: "MAX",
+                    change: 1
+                }
             }),
             m.numeric({
                 name: "remaining",
                 cluster: "genAnalogInput",
                 attribute: "presentValue",
                 description: "The remaining space unoccupied in this parking sensor",
-                access: "STATE_GET"
+                access: "STATE_GET",
+                reporting: {
+                    min: "MIN",
+                    max: "MAX",
+                    change: 1
+                }
             })
         ]
     }
